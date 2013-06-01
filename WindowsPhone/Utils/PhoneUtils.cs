6755be2f192d5a7ecf1ab0942e895f629cfe2967
Denis Stankovski <Denis Stankovski@localhost>
2013-05-31 17:26:39 -0700

    Prototype of new page info layout

---
+++ WindowsPhone/Utils/PhoneUtils.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.IO.IsolatedStorage;
+using System.Windows;
+using Microsoft.Phone.Info;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Phone.Shell;
+
+namespace QuranPhone.Utils
+{
+    public class PhoneUtils
+    {
+        public static string CurrentMemoryUsage()
+        {
+            return string.Format("{0}MB", (double)((long)DeviceExtendedProperties.GetValue("ApplicationCurrentMemoryUsage") / 1024 / 1024));
+        }
+
+        public static string TotalDeviceMemory()
+        {
+            return string.Format("{0}MB", (double)((long)DeviceExtendedProperties.GetValue("DeviceTotalMemory") / 1024 / 1024));
+        }
+
+        private static bool? designTime;
+        public static bool IsDesignMode
+        {
+            get
+            {
+                if (!designTime.HasValue)
+                {
+                    try
+                    {
+                        var isoStor = IsolatedStorageSettings.ApplicationSettings.Contains("asasdasd");
+                        designTime = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        designTime = true;
+                    }
+                }
+                return designTime.Value;
+            }
+        }
+
+        public static void DisableIdleDetection()
+        {
+            try
+            {
+                PhoneApplicationService.Current.ApplicationIdleDetectionMode = IdleDetectionMode.Disabled;
+            }
+            catch (InvalidOperationException ex)
+            {
+                // This exception is expected in the current release.
+            }
+        }
+
+        public static void EnableIdleDetection()
+        {
+            try
+            {
+                PhoneApplicationService.Current.ApplicationIdleDetectionMode = IdleDetectionMode.Enabled;
+            }
+            catch (InvalidOperationException ex)
+            {
+                // This exception is expected in the current release.
+            }
+        }
+    }
+}
---
+++ WindowsPhone/ViewModels/PageViewModel.cs
@@ -0,0 +1,118 @@
+﻿using QuranPhone.Data;
+using System;
+using System.Collections.ObjectModel;
+
+namespace QuranPhone.ViewModels
+{
+    public class PageViewModel : ViewModelBase
+    {
+        public PageViewModel()
+        { }
+
+        public PageViewModel(int page)
+        {
+            PageNumber = page;
+        }
+
+        #region Properties
+
+        private string translation;
+        public string Translation
+        {
+            get { return translation; }
+            set
+            {
+                if (value == translation)
+                    return;
+
+                translation = value;
+
+                base.OnPropertyChanged(() => Translation);
+            }
+        }
+
+        private int pageNumber;
+        public int PageNumber
+        {
+            get { return pageNumber; }
+            set
+            {
+                if (value == pageNumber)
+                    return;
+
+                pageNumber = value;
+
+                base.OnPropertyChanged(() => PageNumber);
+            }
+        }
+
+        private bool showTranslation;
+        public bool ShowTranslation
+        {
+            get { return showTranslation; }
+            set
+            {
+                if (value == showTranslation)
+                    return;
+
+                showTranslation = value;
+
+                base.OnPropertyChanged(() => ShowTranslation);
+            }
+        }
+
+        private int textSize;
+        public int TextSize
+        {
+            get { return textSize; }
+            set
+            {
+                if (value == textSize)
+                    return;
+
+                textSize = value;
+
+                base.OnPropertyChanged(() => TextSize);
+            }
+        }
+
+        private Uri imageSource;
+        public Uri ImageSource
+        {
+            get { return imageSource; }
+            set
+            {
+                if (value == imageSource)
+                    return;
+
+                imageSource = value;
+
+                base.OnPropertyChanged(() => ImageSource);
+            }
+        }
+        
+        public double ScreenWidth
+        {
+            get { return System.Windows.Application.Current.Host.Content.ActualWidth - 20; }
+        }
+
+        // QuranInfo Properties
+        public String SuraName
+        {
+            get { return QuranInfo.GetSuraNameFromPage(PageNumber); }
+        }
+
+        public String JuzName
+        {
+            get { return QuranInfo.GetJuzString(PageNumber); }
+        }
+
+        #endregion Properties
+
+        protected override void OnDispose()
+        {
+            base.OnDispose();
+            ImageSource = null;
+        }
+    }
+}
