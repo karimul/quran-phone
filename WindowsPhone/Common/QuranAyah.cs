71594f0a0553e7efc024a711413014bcbaf987ae
Denis Stankovski <Denis Stankovski@localhost>
2013-07-19 12:01:28 -0700

    Release 0.3.1

---
+++ WindowsPhone/Common/QuranAyah.cs
@@ -0,0 +1,34 @@
+﻿using SQLite;
+
+namespace QuranPhone.Common
+{
+    [Table("verses")]
+    public class QuranAyah
+    {
+        [Column("sura")]
+        public int Sura { get; set; }
+        [Column("ayah")]
+        public int Ayah { get; set; }
+        [Column("text")]
+        public string Text { get; set; }
+        [Ignore]
+        public string Translation { get; set; }
+
+        public QuranAyah() { }
+
+        public QuranAyah(int sura, int ayah)
+        {
+            Sura = sura;
+            Ayah = ayah;
+        }
+    }
+
+    [Table("arabic_text")]
+    public class ArabicAyah : QuranAyah
+    {
+        public ArabicAyah() { }
+
+        public ArabicAyah(int sura, int ayah) : base(sura, ayah)
+        {}
+    }
+}
